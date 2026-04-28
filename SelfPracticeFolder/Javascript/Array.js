323177a5160e468ffe88791a88c134d8c700fe6f
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-04-28 22:20:42 +0530

    Array logic

---
+++ SelfPracticeFolder/Javascript/Array.js
@@ -0,0 +1,19 @@
+let nums = [0,0,1,1,1,2,2,3,3,4];
+
+var removeDuplicates = function(nums) {
+     if (nums.length === 0) return 0;
+
+    let i = 0; 
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+  };
+
+  console.log(removeDuplicates(nums)); // Pass the nums array
+  //console.log(nums); // See the modified array
