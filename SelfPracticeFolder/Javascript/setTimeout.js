0f789c6f4c988502466e00c50ae271fd52ee720d
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-04-28 20:23:13 +0530

    setTimeout program

---
+++ SelfPracticeFolder/Javascript/setTimeout.js
@@ -0,0 +1,19 @@
+function x(){
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function(){
+
+      console.log(i);
+
+    }, i*1000)
+  }
+  console.log("Hello World");
+ }
+
+ x();
+// Output
+//  Hello World
+// 6
+// 6
+// 6
+// 6
+// 6
