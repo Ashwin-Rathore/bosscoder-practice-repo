ff3b591af07ab34938fca1b01e9b2554881bebcd
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-04-28 20:29:30 +0530

    Practicing different options with setTimeout

--- SelfPracticeFolder/Javascript/setTimeout.js
+++ SelfPracticeFolder/Javascript/setTimeout.js
@@ -1,3 +1,11 @@
+// Print 
+// Hello World
+// 1
+// 2
+// 3
+// 4
+// 5
+
 function x(){
   for (var i = 1; i <= 5; i++) {
     setTimeout(function(){
@@ -9,7 +17,7 @@ function x(){
   console.log("Hello World");
  }
 
- x();
+//  x();
 // Output
 //  Hello World
 // 6
@@ -17,3 +25,42 @@ function x(){
 // 6
 // 6
 // 6
+
+
+//  To achieve this Option 1 , use let instead of var
+
+function y(){
+  for(let i =1;i<=5;i++){
+    setTimeout(function(){
+
+      console.log(i);
+
+     }, i*1000)
+  }
+
+  console.log("Hello World");
+}
+
+//y()
+
+//  To achieve this Option 2 , Inclose it inside a function
+
+
+function z(){
+  for (var i =1;i<=5;i++){
+
+    function close(x){
+      setTimeout(function(){
+
+        console.log(x);
+        
+      }, i *1000)
+
+}
+
+close(i);
+  }
+console.log("Hello World");
+}
+
+z();
