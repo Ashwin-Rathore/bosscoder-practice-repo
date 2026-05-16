88d5469765f2eb398aea72592427be0f730eba85
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-16 12:00:42 +0530

    Loops

---
+++ BOSSCODER_Sessions/Session_3(loops_function)/factorOf70.js
@@ -0,0 +1,8 @@
+let n = 70;
+let i = 1;
+while (i <= n / 2) {
+  if (n % i == 0) {
+    console.log(i);
+  }
+  i++;
+}
---
+++ BOSSCODER_Sessions/Session_3(loops_function)/forloop.js
@@ -0,0 +1,14 @@
+
+let oddNum = () => {
+  let count = 0;
+  for(let i=1;;i++){
+    
+    if(count <40 && i%2!=0){
+      console.log(i);
+      count++;
+    }
+  }
+
+}
+
+oddNum();
