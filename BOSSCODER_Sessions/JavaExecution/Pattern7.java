704ae131b238e820281354da2fc014b88c631b7b
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-17 22:18:05 +0530

    Diamond pattern

---
+++ BOSSCODER_Sessions/JavaExecution/Pattern7.java
@@ -0,0 +1,37 @@
+//     *
+//    * *
+//   *   *
+//  *     *
+//   *   *
+//    * *
+//     *
+
+package BOSSCODER_Sessions.JavaExecution;
+
+public class Pattern7 {
+
+    public static void pattern(int row, int column){
+
+        for(int i =0;i<row;i++){
+
+          for(int j =0;j<column;j++){
+                if(i==0 && j<3){
+              System.out.print(" ");
+          }else
+            System.out.print("* ");
+            }
+          System.out.println("");
+        }
+
+    }
+  
+    public static void main(String[] args) {
+
+
+        pattern(7, 7);
+
+
+    }
+
+}
+
