71e0129dca3b324763d94cd07bb5f05de5593639
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-17 12:22:37 +0530

    1st Pattern

--- BOSSCODER_Sessions/Session_4(PatternAndTimeComplex)/Pattern1.js
+++ BOSSCODER_Sessions/Session_4(PatternAndTimeComplex)/Pattern1.js
@@ -1,1 +1,15 @@
-let pat = 20;
+let pat = ()=> {
+
+  let row = 8
+  let column = 3
+  for(let i=0;i<row;i++){
+
+    for(let j=0;j<column;j++){
+      process.stdout.write("* ");
+    }
+    console.log();
+  }
+
+}
+
+pat();
