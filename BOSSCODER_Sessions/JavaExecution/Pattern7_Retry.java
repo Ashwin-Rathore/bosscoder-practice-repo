cc54dca93ee0009e8e51752d1df73efc22d830da
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-19 09:26:25 +0530

    Refactoring code

--- BOSSCODER_Sessions/JavaExecution/Pattern7_Retry.java
+++ BOSSCODER_Sessions/JavaExecution/Pattern7_Retry.java
@@ -14,24 +14,19 @@ public class Pattern7_Retry {
 
     int n =4;
     int temp = n;
-    int newTemp = n;
-   
+    int newTemp = n;  
         for(int i=0;i<=n;i++){
-
                for(int j =0;j<=n*2;j++){
                     if(j==temp || j==newTemp && i!=0 ){
                         System.out.print("* ");
                     }  
                     else
-                        System.out.print("  ");
-                        
+                        System.out.print("  ");                        
                }
                System.out.println();
                temp--;
                newTemp++;   
-                      
         }
-       
                  temp = 1;
                  newTemp = (n * 2) - 1;
             for(int k=n-1;k>0;k--){
@@ -46,12 +41,7 @@ public class Pattern7_Retry {
                System.out.println(); 
                temp++;
                newTemp--;   
-
-            
-         
             }
-        
-
   }
 
 }
