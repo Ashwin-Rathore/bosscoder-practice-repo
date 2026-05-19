6ed35100c792b4c4d40da4698c609cf37f01be6f
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-19 09:24:52 +0530

    Solved Diamond Problem

--- BOSSCODER_Sessions/JavaExecution/Pattern7_Retry.java
+++ BOSSCODER_Sessions/JavaExecution/Pattern7_Retry.java
@@ -12,21 +12,46 @@ public class Pattern7_Retry {
 
   public static void main(String[] args) {
 
-    int n =3;
-    int row=7;
-    int column = 7;
+    int n =4;
+    int temp = n;
+    int newTemp = n;
+   
+        for(int i=0;i<=n;i++){
 
-    
-        for(int i=0;i<n;i++){
-
-               for(int j =0;j<n;j++){
-                    if(j==n ){
-                System.out.print("* ");
-               } else System.out.print(". ");
+               for(int j =0;j<=n*2;j++){
+                    if(j==temp || j==newTemp && i!=0 ){
+                        System.out.print("* ");
+                    }  
+                    else
+                        System.out.print("  ");
+                        
                }
-                System.out.println();
-                n--;
+               System.out.println();
+               temp--;
+               newTemp++;   
+                      
         }
+        // System.out.println("TEMP :"+temp);
+        // System.out.println("NEW_TEMP :"+newTemp);
+         //
+                 temp = 1;
+                 newTemp = (n * 2) - 1;
+            for(int k=n-1;k>0;k--){
+                for(int j =0;j<=n*2;j++){
+                    if(j==temp || j==newTemp){
+                        System.out.print("* ");
+                    }  
+                    else
+                        System.out.print("  ");
+                        
+               }
+               System.out.println(); 
+               temp++;
+               newTemp--;   
+
+            
+         
+            }
         
 
   }
