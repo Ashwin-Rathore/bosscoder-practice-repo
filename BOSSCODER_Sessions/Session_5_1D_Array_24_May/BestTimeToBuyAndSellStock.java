b08994c9548e29ba8a2cec59de1c32a8e254e54c
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-29 21:56:23 +0530

    Pascal Triangle 2

--- BOSSCODER_Sessions/Session_5_1D_Array_24_May/BestTimeToBuyAndSellStock.java
+++ BOSSCODER_Sessions/Session_5_1D_Array_24_May/BestTimeToBuyAndSellStock.java
@@ -2,10 +2,32 @@ package BOSSCODER_Sessions.Session_5_1D_Array_24_May;
 
 public class BestTimeToBuyAndSellStock {
 
-  public static void main(String[] args) {
-    
+      static public int bestTime(int [] prices){
+          int maxProfit = 0;
+          int minPrice = prices[0];
+          for(int i=0;i<prices.length;i++){
+              if(prices[i]< minPrice){
+                  minPrice = prices[i];
+              }
+              int profit = prices[i]-minPrice;
+                if(maxProfit<profit){
+                    maxProfit = profit;
+                }
+          }
 
-    
+
+
+
+
+        return maxProfit;
+      }
+
+  public static void main(String[] args) {
+      int [] prices = {7,1,5,3,6,4};
+
+     System.out.println( bestTime(prices));
+
+
   }
   
 }
--- BOSSCODER_Sessions/Session_5_1D_Array_24_May/PascalTriangle.js
+++ BOSSCODER_Sessions/Session_5_1D_Array_24_May/PascalTriangle.js
@@ -1,5 +1,6 @@
-let n =5;
 
+let rowIndex =3;
+let n =rowIndex+1;
 let fact = (num)=>{
   let sum =1;
   while(num>0){
@@ -10,26 +11,30 @@ let fact = (num)=>{
 }
 
 let ncr = (n,r)=> {
-    return result = fact(n)/(fact(r)*(fact(n-r)))
+    return Math.round(fact(n)/(fact(r)*(fact(n-r))))
 }
 
 let pascalArray = [];
 let pascal =  (n)=> {
-
+let row = [];
   for(let i=0;i< n;i++){
-    let row = [];
+    
     for(let j=0;j<n-i;j++){
         process.stdout.write(" ");
     }
       for(let k=0;k<=i;k++) { 
       process.stdout.write(ncr(i,k).toString()+" ");
+      if(i==rowIndex){
       row.push(ncr(i,k));
-      }
+        }
+    }
+      if(i==rowIndex){
       pascalArray.push(row);
-    console.log();
+      }
+      console.log();
 
   }
-  return pascalArray;
+  return row;
 
 }
 console.log(pascal(n));
