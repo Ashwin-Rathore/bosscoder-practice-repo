25aa50410b96c3473c9c8b68dd9af9abc197b03d
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-05-31 08:59:26 +0530

    Flipong AnImage

--- BOSSCODER_Sessions/Session_6_2S_Array_30_May/FlippingAnImage.js
+++ BOSSCODER_Sessions/Session_6_2S_Array_30_May/FlippingAnImage.js
@@ -1,20 +1,34 @@
 var flipAndInvertImage = function(image) {
-    
+          
         for(let i=0;i<image.length;i++){
-
+            let len = image[i].length-1;
             for (let j=0;j<image[i].length/2;j++){
                 let temp = image[i][j];
-                 image[i][j] =  image[i][image[i].length-j]
-                 image[i][image[i].length-j] = temp;
+                 image[i][j] =  image[i][len-j]
+                 image[i][len-j] = temp;
+
+                
             }
-           
-        }
+          }
+            for(let i=0;i<image.length;i++){
+              for (let j=0;j<image[i].length;j++){
+
+             if(image[i][j] == 0) image[i][j] = 1;
+                 else image[i][j] = 0;
+              }
+            }   
+        
+
+
+        return image;
+
 
 };
 
 let image = [[1,1,0],[1,0,1],[0,0,0]];
+//flipAndInvertImage(image)
 console.log(flipAndInvertImage(image));
-for(let i=0;i<image.length;i++)
-{
-  console.log(image[i])
-}
+// for(let i=0;i<image.length;i++)
+// {
+//   console.log(image[i])
+//}
