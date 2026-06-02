e47d8855eae77f1f3358f8dd37ee3ef594b78b47
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-02 22:46:40 +0530

    cell with odds

---
+++ BOSSCODER_Sessions/Session_6_2S_Array_30_May/CellsWithOddValuesInAMatrix.java
@@ -0,0 +1,44 @@
+package BOSSCODER_Sessions.Session_6_2S_Array_30_May;
+
+public class CellsWithOddValuesInAMatrix {
+
+      static public int cell(int m,int n,int[][] indices){
+        int sum =0;
+        int arr[] [] = new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = 0;
+            }
+        }
+
+        for(int indRow=0;indRow<indices.length;indRow++){
+            for(int indCol=0;indCol<indices[indRow].length;indCol++){
+                  if(indices[indRow][indCol] == 1){
+                      
+                  }
+
+                }
+            }
+        
+
+       
+        // ----- Print -----
+        for(int i = 0; i < arr.length; i++) {
+    for(int j = 0; j < arr[i].length; j++) {
+        System.out.print(arr[i][j] + " ");
+    }
+    System.out.println();
+}
+        // ----- Print -----
+
+        return sum;
+      }
+  public static void main(String[] args) {
+    int m =2;
+    int n =3;
+    int[][] indices = {{0,1},{1,1}};
+    cell(m,n,indices);
+  }
+
+}
---
+++ BOSSCODER_Sessions/Session_6_2S_Array_30_May/CellsWithOddValuesInAMatrix.js
@@ -0,0 +1,20 @@
+var oddCells = function(m, n, indices) {
+    let arr = {};
+    console.log("---"+ typeof arr)
+    for(let i=0;i<m ;i++){
+            
+            for(let j=0;j<n;j++){
+                
+                console.log(arr[i][j]);
+
+            }
+    }
+
+    
+    
+};
+
+let m =2
+let n =3;
+let indices = [[0,1],[1,1]]
+console.log(oddCells(m,n,indices));
