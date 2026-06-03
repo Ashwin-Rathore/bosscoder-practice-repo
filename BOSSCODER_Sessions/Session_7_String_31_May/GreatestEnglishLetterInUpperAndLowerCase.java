8075d386d0ecb815b00089f34c41a0a2f7bd2a3c
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-03 09:08:38 +0530

    GreatestEnglishLetterInUpperAndLowerCase

---
+++ BOSSCODER_Sessions/Session_7_String_31_May/GreatestEnglishLetterInUpperAndLowerCase.java
@@ -0,0 +1,29 @@
+package BOSSCODER_Sessions.Session_7_String_31_May;
+
+import java.util.HashSet;
+
+public class GreatestEnglishLetterInUpperAndLowerCase {
+  //static char cha ;
+    static public String greatestEnglishLetter(String s){
+      
+        char[] ch = s.toCharArray();
+        HashSet<Character> h = new HashSet<>();
+          for(int i=0;i<ch.length;i++){
+              h.add(ch[i]);
+          }
+
+          for(char c = 'Z';c>='A';c--){
+                if(h.contains(c) && h.contains((char)(c+32))){
+                      return Character.toString(c);
+                }
+          }
+
+          return "";
+    }
+
+  public static void main(String[] args) {
+    String s = "lEeTcOdE";
+    System.out.println("---"+greatestEnglishLetter(s));
+  }
+}
+
