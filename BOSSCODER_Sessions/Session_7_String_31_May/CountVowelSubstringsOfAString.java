a0b7670cdac94a4f817058b41e1e735845e58494
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-07 00:05:18 +0530

    count vowel

---
+++ BOSSCODER_Sessions/Session_7_String_31_May/CountVowelSubstringsOfAString.java
@@ -0,0 +1,50 @@
+package BOSSCODER_Sessions.Session_7_String_31_May;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CountVowelSubstringsOfAString {
+
+  static public boolean isVowel(char c){
+   
+      if( c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u'){
+        return true;
+      }
+      return false;
+  }
+
+  static public int countVowelSubString(String s){
+      s.toLowerCase();
+      int count =0;
+
+    for (int start =0;start<s.length();start++) {
+        
+      HashSet<Character> h = new HashSet<>();
+      
+      for(int end=start; end<s.length();end++){
+           
+            char characterSeq = s.charAt(end);
+                 if(!isVowel(characterSeq) ){
+                      break;
+                }else
+                    h.add(characterSeq);
+                if(h.size() == 5){
+                    count++;
+                }
+
+        }
+
+    }
+
+
+
+      return count;
+  }
+  
+  public static void main(String[] args) {
+    
+    String s = "cuaieuouac";
+    System.out.println(countVowelSubString(s));
+
+  }
+}
