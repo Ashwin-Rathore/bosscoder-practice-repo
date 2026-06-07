65b7b827759b90685cd3b9ee1241f21e0d1c50a4
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-07 22:58:14 +0530

    count pair

---
+++ BOSSCODER_Sessions/Session_7_String_31_May/CountPairsOfSimilarStrings.java
@@ -0,0 +1,22 @@
+package BOSSCODER_Sessions.Session_7_String_31_May;
+
+import java.util.HashSet;
+
+public class CountPairsOfSimilarStrings {
+      static public int countPair(String [] words){
+          int count =0;
+          
+
+
+
+
+
+        return count;
+      }
+
+      public static void main(String[] args) {
+        String [] words = {"aba","aabb","abcd","bac","aabc"};
+
+      }
+  
+}
