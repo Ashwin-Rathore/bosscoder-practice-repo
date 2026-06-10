e1425da443229fec97d436f377100295aa753294
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-10 08:07:45 +0530

    bit manipulation

---
+++ SelfPracticeFolder/Java/BitManipulation/Complement.java
@@ -0,0 +1,9 @@
+package SelfPracticeFolder.Java.BitManipulation;
+
+public class Complement {
+  public static void main(String[] args) {
+      System.out.println(~10);
+      System.out.println(~10+1);
+
+  }
+}
---
+++ SelfPracticeFolder/Java/BitManipulation/DataTypes.java
@@ -0,0 +1,8 @@
+package SelfPracticeFolder.Java.BitManipulation;
+
+public class DataTypes {
+  public static void main(String[] args) {
+      byte a   = 127;
+    System.out.println(a);
+  }
+}
