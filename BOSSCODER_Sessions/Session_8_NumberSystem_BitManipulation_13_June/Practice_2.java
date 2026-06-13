e53221af2747340fef68e5df4aa45e62e4f667d5
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-13 15:56:02 +0530

    xor

---
+++ BOSSCODER_Sessions/Session_8_NumberSystem_BitManipulation_13_June/Practice_2.java
@@ -0,0 +1,29 @@
+package BOSSCODER_Sessions.Session_8_NumberSystem_BitManipulation_13_June;
+
+public class Practice_2 {
+    static public double powerFind(int a){
+        double num =0;
+        int count = 0;
+        
+      //   for(int i=0;i<32;i++){
+      //       if (16==(1<<i)) {
+      //         if (count >1) break;
+      //         count ++;
+      //     num =  Math.pow(2, i);
+      //       }
+      //   }
+
+
+
+      // return num;
+   
+   
+   
+   
+    }
+
+  public static void main(String[] args) {
+        int a = 32;
+        System.out.println(powerFind(a));
+  }
+}
