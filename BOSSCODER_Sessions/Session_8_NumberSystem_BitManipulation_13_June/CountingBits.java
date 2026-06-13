4b91b2915773af647ea9f787d5b19190e8953454
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-13 17:58:51 +0530

    counting

---
+++ BOSSCODER_Sessions/Session_8_NumberSystem_BitManipulation_13_June/CountingBits.java
@@ -0,0 +1,30 @@
+package BOSSCODER_Sessions.Session_8_NumberSystem_BitManipulation_13_June;
+
+public class CountingBits {
+
+  static public int[] countBits(int n) {
+    int[] arr = new int[n + 1];
+    for (int i = 0; i <= n; i++) {
+      int temp = i;
+      int counter = 0;
+      while (temp > 0) {
+        if (temp % 2 != 0) {
+          counter++;
+        }
+        temp = temp / 2;
+      }
+      arr[i] = counter;
+    }
+
+    for (int i = 0; i <= n; i++) {
+      System.out.println(arr[i]);
+    }
+
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    int n = 5;
+    System.out.println(countBits(n));
+  }
+}
