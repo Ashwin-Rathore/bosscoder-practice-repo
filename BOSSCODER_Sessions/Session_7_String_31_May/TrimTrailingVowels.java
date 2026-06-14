08272cf5c810d9466cb63b3dd3b852029a4bb6d6
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-14 17:14:23 +0530

    trrim vowel

--- BOSSCODER_Sessions/Session_7_String_31_May/TrimTrailingVowels.java
+++ BOSSCODER_Sessions/Session_7_String_31_May/TrimTrailingVowels.java
@@ -2,23 +2,19 @@ package BOSSCODER_Sessions.Session_7_String_31_May;
 
 public class TrimTrailingVowels {
   static public String trimVowel(String s){
-      String finalStr = "";
+    
+     
       char[] ch = s.toCharArray();
-      StringBuffer sb = new StringBuffer();
-      for(int i=ch.length-1;i>0;i--){
-        if(ch[i]!= 'a' ||ch[i]!= 'e' ||ch[i]== 'i' ||ch[i]== 'o' ||ch[i]== 'u')
-            if(ch[i]== 'a' ||ch[i]== 'e' ||ch[i]== 'i' ||ch[i]== 'o' ||ch[i]== 'u'){
-              s.substring(0, i-1);
-              System.out.println("New String : "+s);
-            }
-          
-            
-      }
-      System.out.println("sb "+ sb);
-      return finalStr;
+      int counter =ch.length-1;
+        for(int i=ch.length-1; i>=0;i--){
+          if(ch[i]== 'a' ||ch[i]== 'e' ||ch[i]== 'i' ||ch[i]== 'o' ||ch[i]== 'u') counter--;
+          else break;
+
+        }
+      return s.substring(0, counter+1);
   }
   public static void main(String[] args) {
-    String s = "idea";
+    String s = "day";
     System.out.println(trimVowel(s));
   }
 }
