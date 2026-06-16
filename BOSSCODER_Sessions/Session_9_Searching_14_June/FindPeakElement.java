d369ba8aff14422149b82b0ad5c1703367aecf9c
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-16 09:32:02 +0530

    element first

--- BOSSCODER_Sessions/Session_9_Searching_14_June/FindPeakElement.java
+++ BOSSCODER_Sessions/Session_9_Searching_14_June/FindPeakElement.java
@@ -20,7 +20,7 @@ public class FindPeakElement {
     }
      public static void main(String[] args) {
     
-    int[] arr = {1,3,5,5,5,999,67,123,155};
+    int[] arr = {1,2,1,3,5,6,4};
     System.out.println(findPeakElement(arr));
   }
 
