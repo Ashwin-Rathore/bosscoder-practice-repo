6358d2797b34ca03f4f6440c0f2f23ede868ff87
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-16 09:31:34 +0530

    FindPeakElement

---
+++ BOSSCODER_Sessions/Session_9_Searching_14_June/FindPeakElement.java
@@ -0,0 +1,27 @@
+package BOSSCODER_Sessions.Session_9_Searching_14_June;
+
+public class FindPeakElement {
+
+   public static  int findPeakElement(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+     public static void main(String[] args) {
+    
+    int[] arr = {1,3,5,5,5,999,67,123,155};
+    System.out.println(findPeakElement(arr));
+  }
+
+}
