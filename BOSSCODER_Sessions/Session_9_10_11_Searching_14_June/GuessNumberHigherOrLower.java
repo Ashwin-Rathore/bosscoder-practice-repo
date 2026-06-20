151b7c98525d78fcd36671401f50a4c21af6ba6d
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-06-20 12:13:43 +0530

    leetcode 374

--- BOSSCODER_Sessions/Session_9_10_11_Searching_14_June/GuessNumberHigherOrLower.java
+++ BOSSCODER_Sessions/Session_9_10_11_Searching_14_June/GuessNumberHigherOrLower.java
@@ -1,13 +1,26 @@
 package BOSSCODER_Sessions.Session_9_10_11_Searching_14_June;
 
 public class GuessNumberHigherOrLower {
-  public static int guessNumber(int n) {
-    int num = 0;
+   public int guessNumber(int n) {
+
+        if(n==1) return n;
+        int l = 0;
+        int r = n;
+
+        while(l<=r){
+            int mid = r - (r-l)/2;
+            int result = 0;
+            // int result = guess(mid); - This step will not work as needs api fof guess from leetcode 374 qs
+            
+            
+            if(result == 0) return mid;
+            else if(result == -1) r = mid-1;
+            else l = mid+1;
 
 
-    
-    return num;
-  }
+        }
+        return -1;
+    }
 
   public static void main(String[] args) {
     
