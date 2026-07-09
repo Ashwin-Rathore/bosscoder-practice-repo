3acdbf47bf38fecac508f4a15c896a6ce14840ee
Ashwin Rathore <ashwin.rathore001@gmail.com>
2026-07-09 20:57:36 +0530

    minimum

---
+++ BOSSCODER_Sessions/Session_5_1D_Array_24_May/MinimumSwapsToMoveZerosToEnd.java
@@ -0,0 +1,29 @@
+package BOSSCODER_Sessions.Session_5_1D_Array_24_May;
+
+public class MinimumSwapsToMoveZerosToEnd {
+  
+    public static int minSwap(int[] nums){
+        int i =0;
+        int j = nums.length-1;
+        int count =0;
+        while(i<j){
+            if(nums[j]==0) j--;
+            if(nums[i]==0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                count++;
+                j--;
+            }
+                i++;
+                
+        }
+      return count;
+    }
+  public static void main(String[] args) {
+    
+    int[] nums = {1,2,0};
+    System.out.println(minSwap(nums ));
+
+  }
+}
